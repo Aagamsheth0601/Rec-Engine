71784b54288ecb409d85c795d1e1a680b9a0124b
dhiraj2301 <dhirajshetty91@yahoo.com>
2023-03-04 17:31:27 +0530

    Modal added to onMouseDown

--- rec-engine/src/components/interactive.js
+++ rec-engine/src/components/interactive.js
@@ -1,14 +1,30 @@
 import Spline from "@splinetool/react-spline";
+import ModalTemplate from "./modalTemplate";
+import { useState } from 'react';
+
 
 export default function Interactive() {
+  const [modalShow, setModalShow] = useState(false)
+
+  function onMouseDown(e) {
+    console.log(e.target.name);
+    setModalShow(true);
+  }
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "800px",
-      }}
-    >
-      <Spline scene="https://prod.spline.design/49Id9tLwIHZ-xsa3/scene.splinecode" />
-    </div>
+    <>
+
+      <ModalTemplate
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+      <div
+        style={{
+          width: "100%",
+          height: "800px",
+        }}
+      >
+        <Spline scene="https://prod.spline.design/49Id9tLwIHZ-xsa3/scene.splinecode" onMouseDown={onMouseDown} />
+      </div>
+    </>
   );
 };
