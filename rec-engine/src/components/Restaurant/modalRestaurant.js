2bb24b52e3b66194b177ffba363d0a56bba76f04
Aagamsheth0601 <aagamsheth0601@gmail.com>
2023-04-03 11:04:02 +0530

    WIP: Search bar

--- rec-engine/src/components/Restaurant/modalRestaurant.js
+++ rec-engine/src/components/Restaurant/modalRestaurant.js
@@ -1,60 +1,112 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import ModalRestaurantBasedOnCusine from './modalRestaurantBasedOnCusine'
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import ModalRestaurantBasedOnCusine from "./modalRestaurantBasedOnCusine";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
 // import Cusine from './cusine'
 
-
 export default function ModalRestaurant(props) {
-    const [modalRestaurantBasedOnCusine, setModalRestaurantBasedOnCusine] = useState(false);
-    const [cusineSelected, setCusineSelected] = useState(null);
-    const [data, setData] = useState([]);
+  const [modalRestaurantBasedOnCusine, setModalRestaurantBasedOnCusine] =
+    useState(false);
+  const [cusineSelected, setCusineSelected] = useState(null);
+  const [data, setData] = useState([]);
+  const [filteredInfo, setFilteredInfo] = useState([]);
+  const [search, setSearch] = useState("");
 
-    useEffect(() => {
-        axios.get('http://127.0.0.1:8000/restaurant/all_cusine?longitude=' + props.longitude + '&latitude=' + props.latitude).then(response => {
-            setData(response.data);
-        }).catch(err => {
-            console.log(err);
-        });
-    }, [props.latitude, props.longitude]);
+  const handleChange = (e) => {
+    const searchText = e.target.value;
+    console.log(searchText);
+    setSearch(searchText);
+    if (searchText !== "") {
+      const xyz = data.filter((item) => {
+        if (item.includes(searchText)) {
+          console.log(item);
+          return item;
+        }
+      });
+      console.log(xyz);
+      setFilteredInfo(xyz);
+    } else {
+      setFilteredInfo(data);
+    }
+  };
 
-    return (
-        <>
+  useEffect(() => {
+    axios
+      .get(
+        "http://127.0.0.1:8000/restaurant/all_cusine?longitude=" +
+          props.longitude +
+          "&latitude=" +
+          props.latitude
+      )
+      .then((response) => {
+        setData(response.data.sort());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setFilteredInfo(data);
+  }, [props.latitude, props.longitude]);
 
-            <ModalRestaurantBasedOnCusine
-                show={modalRestaurantBasedOnCusine}
-                onHide={() => { setCusineSelected(null); props.setModalRestaurant(true); setModalRestaurantBasedOnCusine(false) }}
-                cusine={cusineSelected}
-                setModalRestaurantBasedOnCusine={setModalRestaurantBasedOnCusine}
-                longitude={props.longitude}
-                latitude={props.latitude}
+  return (
+    <>
+      <ModalRestaurantBasedOnCusine
+        show={modalRestaurantBasedOnCusine}
+        onHide={() => {
+          setCusineSelected(null);
+          props.setModalRestaurant(true);
+          setModalRestaurantBasedOnCusine(false);
+        }}
+        cusine={cusineSelected}
+        setModalRestaurantBasedOnCusine={setModalRestaurantBasedOnCusine}
+        longitude={props.longitude}
+        latitude={props.latitude}
+      />
+
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            All Cusines available near your location
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <TextField
+              value={search}
+              placeholder="Search"
+              onChange={(e) => {
+                handleChange(e);
+              }}
             />
 
-            <Modal
-                {...props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        All Cusines available near your location
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div>
-                        {data.map(item => (
-                            <Button variant="light" onClick={() => { props.setModalRestaurant(false); setCusineSelected(item); setModalRestaurantBasedOnCusine(true); }}>
-                                {item}
-                            </Button>
-                        ))}
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={props.onHide}>Close</Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
+            {filteredInfo.map((item) => (
+              <>
+                <Button
+                  variant="light"
+                  onClick={() => {
+                    props.setModalRestaurant(false);
+                    setCusineSelected(item);
+                    setModalRestaurantBasedOnCusine(true);
+                  }}
+                >
+                  {item}
+                </Button>
+              </>
+            ))}
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
--- rec-engine/src/components/interactive.js
+++ rec-engine/src/components/interactive.js
@@ -1,9 +1,7 @@
 import Spline from "@splinetool/react-spline";
 import ModalTemplate from "./modalTemplate";
 import ModalRestaurant from "./Restaurant/modalRestaurant";
-import React, { useState } from 'react';
-
-
+import React, { useState } from "react";
 
 export default function Interactive() {
   const [modalShow, setModalShow] = useState(false);
@@ -11,26 +9,23 @@ export default function Interactive() {
 
   function onMouseDown(e) {
     console.log(e.target.name);
-    if (e.target.name === 'CheckPointRestaurants') {
+    if (e.target.name === "CheckPointRestaurants") {
       setModalRestaurant(true);
     } else {
       setModalShow(true);
     }
   }
+
   return (
     <>
-
-      <ModalTemplate
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
+      <ModalTemplate show={modalShow} onHide={() => setModalShow(false)} />
 
       <ModalRestaurant
         show={modalRestaurant}
         onHide={() => setModalRestaurant(false)}
         setModalRestaurant={setModalRestaurant}
         longitude={72.828059}
-        latitude={18.942880}
+        latitude={18.94288}
       />
 
       <div
@@ -39,8 +34,11 @@ export default function Interactive() {
           height: "800px",
         }}
       >
-        <Spline scene="https://prod.spline.design/49Id9tLwIHZ-xsa3/scene.splinecode" onMouseDown={onMouseDown} />
+        <Spline
+          scene="https://prod.spline.design/49Id9tLwIHZ-xsa3/scene.splinecode"
+          onMouseDown={onMouseDown}
+        />
       </div>
     </>
   );
-};
+}
